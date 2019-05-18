96dfc8044e12160ca65d1f1ec99d39d67f01a31d
William Sang <sang.williams@gmail.com>
2019-05-18 13:39:07 +0800

    feat: 使用注解给 MongoDB 添加注解

--- src/main/java/fun/canyu/api/model/User.java
+++ src/main/java/fun/canyu/api/model/User.java
@@ -4,15 +4,19 @@ import lombok.Data;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Set;
 
 @Data
 @ToString
 @Accessors(chain = true)
+@Document
 public class User {
     @Id
     private String id;
+    @Indexed(unique = true)
     private String username;
     private String password;
     // 权限字符串存储
