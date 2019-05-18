538debc9135ed5af7b59aae7f8c6568408024461
William Sang <sang.williams@gmail.com>
2019-05-18 12:12:17 +0800

    refactor: 减少 service 层级

--- src/main/java/fun/canyu/api/service/UserService.java
+++ src/main/java/fun/canyu/api/service/UserService.java
@@ -1,7 +1,43 @@
 package fun.canyu.api.service;
 
-public interface UserService {
-    boolean login(String username, String password);
+import com.google.common.collect.ImmutableSet;
+import fun.canyu.api.model.Role;
+import fun.canyu.api.model.User;
+import fun.canyu.api.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-    boolean signUp(String username, String password);
+@Slf4j
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    public boolean login(String username, String password) {
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (Exception ex) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean signUp(String username, String password) {
+        User user = new User()
+                .setUsername(username)
+                .setPassword(passwordEncoder.encode(password))
+                .setRoles(ImmutableSet.of(Role.ROLE_NormalUser));
+        userRepository.insert(user);
+        return true;
+    }
 }
